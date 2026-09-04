a665405acd31a0c4a153ea141d40d040b8c1c90d
kidrocks1313-ui <kidrocks1313@gmail.com>
2026-09-04 14:23:23 -0400

    Update performance.js

--- src/performance.js
+++ src/performance.js
@@ -1,8 +1,12 @@
 const logger = require(__dirname + '/logger').getLogger('performance', 'orange')
-const usage = require('usage')
 
 setInterval(() => {
-  usage.lookup(process.pid, (err, { memory, cpu }) => {
-    logger.debug(`Memory usage: ${memory}  CPU usage: ${cpu.toFixed(3)}`)
-  })
+  const memory = process.memoryUsage()
+  const cpu = process.cpuUsage()
+
+  const memoryMB = (memory.rss / 1024 / 1024).toFixed(2)
+
+  logger.debug(
+    `Memory usage: ${memoryMB} MB  CPU usage: ${cpu.user} user / ${cpu.system} system`
+  )
 }, 5 * 60 * 1000)
